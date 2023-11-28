fa8d132f504c473cd7f7a1095618db8004ec257e
Karthickseetha <karthickseetha2@gmail.com>
2023-11-28 21:13:42 +0530

    Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+export  const projectData=[
+    {
+        id:1,
+        title:"React Tour Website",
+        image:"/assets/tourproject.png",
+        gitUrl:"https://github.com/Karthickseetha/react-backroads-site",
+        previewUrl:'https://venerable-torte-68794f.netlify.app/',
+    },
+    {
+        id:2,
+        title:"React Jobfinder Website",
+        image:"/assets/jobsite.png",
+        gitUrl:"https://github.com/Karthickseetha/React-job-finder-app",
+        previewUrl:'https://fluffy-pudding-6283b7.netlify.app/',
+        
+    },
+    {
+        id:3,
+        title:"React Portfolio Website",
+        image:"/assets/port.png",
+        gitUrl:'https://github.com/Karthickseetha',
+        previewUrl:'',
+
+        
+    },
+    {
+        id:4,
+        title:"React CRUD",
+        image:"/assets/crud.png",
+        gitUrl:"https://github.com/Karthickseetha/React-Crud",
+        previewUrl:'https://clever-cocada-e42583.netlify.app/',
+    },
+    {
+        id:5,
+        title:"React Birthday Remainder",
+        image:"/assets/remainder.png",
+        gitUrl:"https://github.com/Karthickseetha/Birthday-remainder-app",
+        previewUrl:'https://charming-faun-cb9e2c.netlify.app/',
+    },
+    {
+        id:6,
+        title:"React Expense Tracker",
+        image:"/assets/expense.png",
+        gitUrl:'https://github.com/Karthickseetha/React-Expense-Tracker',
+        previewUrl:'https://meek-khapse-98333c.netlify.app/',
+    },
+
+ ]
